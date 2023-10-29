7312a04a204dfac1e733d5c6e19e4d23cef9a46a
Hamdaoui Rogaya <74892452+Rogaya94@users.noreply.github.com>
2023-10-29 23:50:21 +0100

    ajout du formulaire new trick

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Tricks;
-use App\Repository\TricksRepository;
+use App\Repository\TricksRepository;;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,10 +25,33 @@ class TricksController extends AbstractController
     ]);
   }
 
-  #[Route('tricks/{id}', name: 'app_tricks_show')]
+  #[Route('/tricks/new', name: 'app_tricks_new')]
+  public function create(Request $request): Response
+  {
+
+    $trick = new Tricks();
+    $form = $this->createFormBuilder($trick)
+      ->add('title')
+      ->add('content')
+      ->add('image')
+      ->add('video')
+      ->add('groupeTrick')
+
+      ->getForm();
+
+
+
+    return  $this->render('tricks/create.html.twig',
+  [ 
+    'formTrick' => $form->createView()
+    
+  ]);
+  }
+
+  #[Route('/tricks/{id}', name: 'app_tricks_show')]
   public function show(TricksRepository $repo, $id): Response
   {
-    
+
     $trick = $repo->find($id);
     return $this->render('tricks/show.html.twig', [
       'trick' => $trick
