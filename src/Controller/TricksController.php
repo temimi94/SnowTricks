a3263885e5ad2a1bcf0231322e11c08ba80ad6f6
Hamdaoui Rogaya <74892452+Rogaya94@users.noreply.github.com>
2023-10-30 22:12:39 +0100

    ajout de la route pour la modification de trick

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Tricks;
-use App\Repository\TricksRepository;;
+use App\Form\TrickType;
+use App\Repository\TricksRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +16,6 @@ class TricksController extends AbstractController
   #[Route('/', name: 'app_tricks')]
   public function index(TricksRepository $repo): Response
   {
-    //repo = $this->getDoctrine()->getRepository(Tricks::class);
 
     $tricks = $repo->findAll();
 
@@ -24,28 +25,36 @@ class TricksController extends AbstractController
       'tricks' => $tricks
     ]);
   }
+  #[Route("/tricks/{id}/edit", name: "app_tricks_edit")]
+  #[Route("/tricks/new", name: "app_tricks_new")]
 
-  #[Route('/tricks/new', name: 'app_tricks_new')]
-  public function create(Request $request): Response
+  public function form(Tricks $trick = null, Request $request, EntityManagerInterface  $manager): Response
   {
+    if (!$trick) {
+      $trick = new Tricks();
+    }
 
-    $trick = new Tricks();
-    $form = $this->createFormBuilder($trick)
-      ->add('title')
-      ->add('content')
-      ->add('image')
-      ->add('video')
-      ->add('groupeTrick')
+    $form = $this->createForm(TrickType::class, $trick);
 
-      ->getForm();
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      if (!$trick->getId()) {
+        $trick->setCreatedAt(new \DateTime());
+      }
 
+      $manager->persist($trick);
+      $manager->flush();
+      return $this->redirectToRoute('app_tricks_show', ['id' => $trick->getId()]);
+    }
 
+    return  $this->render(
+      'tricks/create.html.twig',
+      [
+        'formTrick' => $form->createView(),
+        'editMode' => $trick->getId() !== null
 
-    return  $this->render('tricks/create.html.twig',
-  [ 
-    'formTrick' => $form->createView()
-    
-  ]);
+      ]
+    );
   }
 
   #[Route('/tricks/{id}', name: 'app_tricks_show')]
---
+++ src/Form/TrickType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tricks;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('groupeTrick')
+            ->add('image')
+            ->add('video')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+        ]);
+    }
+}
